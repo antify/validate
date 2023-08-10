76249024caa6cc821590046a13e27dba4baa1941
Johannes Beyer <johannes@blue-panda.de>
2023-08-10 11:27:12 +0200

    feat: add more types to typeOf rule

--- src/rules/__tests__/isTypeOf.rule.test.ts
+++ src/rules/__tests__/isTypeOf.rule.test.ts
@@ -1,42 +1,99 @@
-import { describe, test, expect } from 'vitest'
-import { isTypeOfRule, Types } from '../isTypeOf.rule'
+import {describe, test, expect} from 'vitest'
+import {isTypeOfRule, Types} from '../isTypeOf.rule'
 
 describe('Is type of rule test', async () => {
+    const listOfStrings = [
+        '',
+        ' ',
+        'content',
+        'true',
+        '1'
+    ];
+    const listOfNumbers = [
+        10,
+        0,
+        0.1,
+        0.0,
+    ];
+    const listOfBooleans = [
+        true,
+        false
+    ];
+
+    function testFunction() {
+
+    }
+
+    const listOfFunctions = [
+        () => '',
+        testFunction
+    ];
+    const listOfObjects = [
+        {with: 'property'},
+        {},
+    ];
+    const listOfArrays = [
+        [],
+        ['with content'],
+    ];
+
     test('should validate type of string correctly', () => {
         [
-            10,
-            0,
-            0.1,
-            0.0,
-            [],
-            ['with content'],
-            { with: 'property' },
-            {},
-            () => ''
-        ].forEach(value => expect(isTypeOfRule(value, Types.STRING)).toBe(`${value} is not type of ${Types.STRING}`));
+            ...listOfObjects,
+            ...listOfArrays,
+            ...listOfFunctions,
+            ...listOfBooleans,
+            ...listOfNumbers
+        ].forEach(value => expect(isTypeOfRule(value, Types.STRING)).toBe(`${JSON.stringify(value)} is not type of ${Types.STRING}`));
 
-        [
-            '',
-            ' ',
-            'content'
-        ].forEach(value => expect(isTypeOfRule(value, Types.STRING)).toBe(true));
+        listOfStrings.forEach(value => expect(isTypeOfRule(value, Types.STRING)).toBe(true));
+    });
 
+    test('should validate type of number correctly', () => {
         [
-            '',
-            ' ',
-            'content',
-            [],
-            ['with content'],
-            { with: 'property' },
-            {},
-            () => ''
-        ].forEach(value => expect(isTypeOfRule(value, Types.NUMBER)).toBe(`${value} is not type of ${Types.NUMBER}`));
+            ...listOfObjects,
+            ...listOfArrays,
+            ...listOfFunctions,
+            ...listOfBooleans,
+            ...listOfStrings
+        ].forEach(value => expect(isTypeOfRule(value, Types.NUMBER)).toBe(`${JSON.stringify(value)} is not type of ${Types.NUMBER}`));
 
+        listOfNumbers.forEach(value => expect(isTypeOfRule(value, Types.NUMBER)).toBe(true));
+    });
+
+    test('should validate type of boolean correctly', () => {
         [
-            10,
-            0,
-            0.1,
-            0.0,
-        ].forEach(value => expect(isTypeOfRule(value, Types.NUMBER)).toBe(true));
-    })
+            ...listOfObjects,
+            ...listOfArrays,
+            ...listOfFunctions,
+            ...listOfStrings,
+            ...listOfNumbers
+        ].forEach(value => expect(isTypeOfRule(value, Types.BOOLEAN)).toBe(`${JSON.stringify(value)} is not type of ${Types.BOOLEAN}`));
+
+        listOfBooleans.forEach(value => expect(isTypeOfRule(value, Types.BOOLEAN)).toBe(true));
+    });
+
+    test('should validate type of object correctly', () => {
+        [
+            ...listOfArrays,
+            ...listOfFunctions,
+            ...listOfStrings,
+            ...listOfNumbers,
+            ...listOfBooleans
+        ].forEach(value => expect(isTypeOfRule(value, Types.OBJECT)).toBe(`${JSON.stringify(value)} is not type of ${Types.OBJECT}`));
+
+        listOfObjects.forEach(value => expect(isTypeOfRule(value, Types.OBJECT)).toBe(true));
+    });
+
+    test('should validate type of object correctly', () => {
+        [
+            ...listOfObjects,
+            ...listOfFunctions,
+            ...listOfStrings,
+            ...listOfNumbers,
+            ...listOfBooleans
+        ].forEach(value => expect(isTypeOfRule(value, Types.ARRAY)).toBe(`${JSON.stringify(value)} is not type of ${Types.ARRAY}`));
+
+        listOfArrays.forEach(value => expect(isTypeOfRule(value, Types.ARRAY)).toBe(true));
+    });
 })
--- src/rules/isTypeOf.rule.ts
+++ src/rules/isTypeOf.rule.ts
@@ -1,5 +1,22 @@
 export enum Types {
     STRING = 'string',
-    NUMBER = 'number'
+    NUMBER = 'number',
+    BOOLEAN = 'boolean',
+    OBJECT = 'object',
+    ARRAY = 'array',
 }
-export const isTypeOfRule = (val: any, type: Types, message: string = `${val} is not type of ${type}`): true | string => typeof val === type || message;
+export const isTypeOfRule = (val: any, type: Types, message: string = `${JSON.stringify(val)} is not type of ${type}`): true | string => {
+    if (typeof val === Types.OBJECT) {
+        if (type === Types.ARRAY && Array.isArray(val)) {
+            return true;
+        }
+
+        if (type === Types.OBJECT && !Array.isArray(val)) {
+            return true;
+        }
+
+        return message;
+    }
+
+    return typeof val === type ? true : message;
+};
