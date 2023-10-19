ad03cca5a3d6d5b82861602dff518eab6b5a3d04
Johannes Beyer <johannes@blue-panda.de>
2023-10-19 13:40:22 +0200

    feat: improve error messages, fix eslint errors

---
+++ src/__tests__/useFormValidator.test.ts
@@ -0,0 +1,108 @@
+import { describe, test, expect, beforeEach } from 'vitest'
+import { ref } from 'vue'
+import { useFormValidator, FormValidator } from '../useFormValidator'
+
+describe('FormValidator test', () => {
+  let formValidator: FormValidator
+
+  beforeEach(() => {
+    formValidator = useFormValidator({
+      truthy: {
+        readableName: 'Truthy field',
+        rules: [() => true, () => true, () => true]
+      },
+      falsy: {
+        readableName: 'Falsy field',
+        rules: [() => 'Message']
+      },
+      truthyAndFalsy: {
+        readableName: 'Truthy and falsy field',
+        rules: [() => true, () => 'Message']
+      },
+      falsyAndFalsy: {
+        readableName: 'Falsy and falsy field',
+        rules: [() => 'Message', () => 'Message']
+      },
+      dynamic: {
+        readableName: 'Dynamic field',
+        rules: [(val: unknown) => val === true || 'Message']
+      }
+    })
+  })
+
+  test('should validate a property correctly', () => {
+    formValidator.validateProperty('truthy', true)
+    expect(formValidator.getErrors()).toStrictEqual({})
+    expect(formValidator.hasErrors()).toBeFalsy()
+
+    formValidator.validateProperty('falsy', true)
+    expect(formValidator.getErrors()).toStrictEqual({
+      falsy: ['Message']
+    })
+    expect(formValidator.hasErrors()).toBeTruthy()
+  })
+
+  test('should validate all properties correctly', () => {
+    formValidator.validate({
+      truthy: true,
+      falsy: true,
+      truthyAndFalsy: true,
+      dynamic: true
+    })
+
+    expect(formValidator.getErrors()).toStrictEqual({
+      falsy: [
+        'Message'
+      ],
+      truthyAndFalsy: [
+        'Message'
+      ],
+      falsyAndFalsy: [
+        'Message',
+        'Message'
+      ]
+    })
+    expect(formValidator.getErrorsAsString()).toStrictEqual('Falsy field\n- Message\nTruthy and falsy field\n- Message\nFalsy and falsy field\n- Message\n- Message')
+  })
+
+  test('should validate reactive types correctly', () => {
+    const _formValidator = useFormValidator({
+      truthy: {
+        rules: [val => val === true || '']
+      },
+      falsy: {
+        rules: [val => val === false || '']
+      }
+    })
+
+    _formValidator.validate({
+      truthy: ref(true),
+      falsy: ref(false)
+    })
+
+    expect(_formValidator.hasErrors()).toBeFalsy()
+  })
+
+  test('should validate all properties with object as param only', () => {
+    expect.assertions(5);
+
+    [true, 'string', 0, 0.0, []].forEach((val: unknown) => {
+      try {
+        // @ts-ignore
+        formValidator.validate(val)
+      } catch (e) {
+        expect(e.message).toBe(`Can not validate data of type ${typeof val}`)
+      }
+    })
+  })
+
+  test('should not validate a not existing property', () => {
+    expect.assertions(1)
+
+    try {
+      formValidator.validateProperty('notExistingOne', true)
+    } catch (e) {
+      expect(e.message).toBe('There is no field for property notExistingOne')
+    }
+  })
+})
---
+++ src/useFormValidator.ts
@@ -0,0 +1,70 @@
+import { Validator } from './useValidator'
+import { Field } from './types'
+
+export class FormValidator {
+  readonly validatorMap: Record<string, Validator> = {}
+  readonly fieldMap: Record<string, Field> = {}
+
+  constructor (fields: Record<string, Field>) {
+    Object.keys(fields).forEach((key) => {
+      this.validatorMap[key] = new Validator(fields[key].rules)
+      this.fieldMap[key] = fields[key]
+    })
+  }
+
+  validate (data: Record<string, unknown | { value: unknown }>) {
+    Object.keys(this.validatorMap).forEach((key: string) => {
+      if (typeof data !== 'object' || Array.isArray(data) || !data) {
+        throw new Error(`Can not validate data of type ${typeof data}`)
+      }
+
+      this.validateProperty(key, data[key])
+    })
+  }
+
+  validateProperty (
+    property: string,
+    data: unknown | { value: unknown }
+  ) {
+    if (!this.validatorMap[property]) {
+      throw new Error(`There is no field for property ${property}`)
+    }
+
+    this.validatorMap[property].validate(data)
+  }
+
+  hasErrors (): boolean {
+    return Object.values(this.validatorMap).some(validator => validator.hasErrors())
+  }
+
+  getErrors (): Record<string, string[]> {
+    const errorMap = {}
+
+    Object.keys(this.validatorMap).forEach((key) => {
+      if (this.validatorMap[key].hasErrors()) {
+        errorMap[key] = this.validatorMap[key].getErrors()
+      }
+    })
+
+    return errorMap
+  }
+
+  getErrorsAsString (): string {
+    const newLine = '\n'
+    let stringMessage = ''
+
+    Object.keys(this.validatorMap).forEach((key) => {
+      if (this.validatorMap[key].hasErrors()) {
+        if (stringMessage) {
+          stringMessage += newLine
+        }
+
+        stringMessage += `${this.fieldMap[key].readableName || key}${newLine}${this.validatorMap[key].getErrorsAsString()}`
+      }
+    })
+
+    return stringMessage
+  }
+}
+
+export const useFormValidator = (fields: Record<string, Field>) => new FormValidator(fields)
